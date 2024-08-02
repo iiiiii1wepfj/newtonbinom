3f20c6269c4d52b07b69e00cbde640ba4f45890b
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2024-08-02 12:20:14 +0300

    Create newtonbinom.py

---
+++ newtonbinom.py
@@ -0,0 +1,16 @@
+def newtonbinom(n):
+	binomcalc = 1
+	expr = []
+	for k in range(0, n+1):
+		templist = []
+		nminusk = n-k
+		if k != 0:
+			binomcalc *= (nminusk+1)/k
+		if binomcalc != 1:
+			templist.append(f"{int(binomcalc)}")
+		if nminusk != 0:
+			templist.append(f"a{'' if nminusk == 1 else f'^{nminusk}'}")
+		if k != 0:
+			templist.append(f"b{'' if k == 1 else f'^{k}'}")
+		expr.append("*".join(templist))
+	return " + ".join(expr)
